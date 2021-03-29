2c7d6fee08deb1eb7c20e8a6afd518f054fe8e28
Dolzhikov1998 <ilyadolzhkv@gmail.com>
2021-03-29 17:59:58 +0300

    change port

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(express.static('card'));
 
 useControllers()
 
-const port = process.env.port || 3000
+const port = process.env.port || 3005
 
 app.listen(port, ()=>{
     console.log("Server run...");

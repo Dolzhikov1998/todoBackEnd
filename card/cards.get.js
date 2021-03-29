045a5dacce7586df2290bee7011d0afac3e4afb7
Dolzhikov1998 <ilyadolzhkv@gmail.com>
2021-03-29 16:40:29 +0300

    add dotenv

--- card/cards.get.js
+++ card/cards.get.js
@@ -7,7 +7,6 @@ const filePath = 'cards.json'
 const router = Router.get('/', function (req, res){
     const content  = fs.readFileSync(filePath, 'utf8')
     const cards = JSON.parse(content)
-    console.log('sdfsdfs')
     if(cards.length > 0) return res.send(cards)
     
     res.status(404).send("Content not found")

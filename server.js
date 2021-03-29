ec76272dda7f8534d15c4bb39797cd115d4f32cd
Dolzhikov1998 <ilyadolzhkv@gmail.com>
2021-03-29 18:19:42 +0300

    Change port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const morgan = require('morgan');
 const klawSync = require('klaw-sync');
 const path = require('path')
-const dotenv = require('dotenv');
+// const dotenv = require('dotenv');
 
 const app = express()
 
@@ -12,7 +12,7 @@ const cors = require('cors')
 app.use(morgan('tiny'));
 app.use(cors())
 
-dotenv.config()
+// dotenv.config()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -41,7 +41,7 @@ app.use(express.static('card'));
 
 useControllers()
 
-const port = process.env.port || 5001
+const port = process.env.PORT || 3000
 
 app.listen(port, ()=>{
     console.log("Server run...");

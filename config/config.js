aa2343ac970e46eb159be98ce9bd9187f5ef8110
Dolzhikov1998 <ilyadolzhkv@gmail.com>
2021-04-05 12:14:32 +0300

    Change config.js

--- config/config.js
+++ config/config.js
@@ -3,6 +3,14 @@ module.exports = {
     "database": "d6k84om0ofqveo",
     "host": "ec2-54-155-87-214.eu-west-1.compute.amazonaws.com",
     "dialect": "postgres",
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
+    "username": 'fnholjatfgwlvu',
+    "password": '65e9f77f66fbe1354efa916f8ee68b7ca032d05bbc8a80d1e1da8cc153bf3764',
+    "port": 5432,
+    "URI": process.env.DATABASE_URL,
+    "dialectOptions": {
+        "ssl": {
+          "require": true,
+          "rejectUnauthorized": false
+        }
+      }
 }

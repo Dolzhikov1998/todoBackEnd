8da184d5dae05fd6458853f56bb1ac9e8153c343
Dolzhikov1998 <ilyadolzhkv@gmail.com>
2021-04-06 11:06:34 +0300

    change for test get request

--- card/cards.get.js
+++ card/cards.get.js
@@ -4,22 +4,22 @@ const { Task } = require('../models')
 
 const router = Router.get('/card', async (req, res) => {
 
-    // const param = {
-    //     where: {},
-    //     order: [],
-    //     offset: req.query.page * 5,
-    //     limit: 5
-    // }
+    const param = {
+        where: {},
+        order: [],
+        offset: req.query.page * 5,
+        limit: 5
+    }
     
-    // if (req.query.done)
-    //     param.where = { done: req.query.done }
-    // if (req.query.order)
-    //     param.order.push(['createdAt', `${req.query.order}`])
+    if (req.query.done)
+        param.where = { done: req.query.done }
+    if (req.query.order)
+        param.order.push(['createdAt', `${req.query.order}`])
 
 
-    // const cards = await Task.findAndCountAll(param)
+    const cards = await Task.findAndCountAll(param)
 
-    res.send("sadasdasdasd")
+    res.send(cards)
 
 })
 
